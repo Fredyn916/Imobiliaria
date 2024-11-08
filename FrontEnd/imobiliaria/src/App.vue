762e67015080624f049bdd85a803c86c214e6057
guilhermesantos78 <163909949+guilhermesantos78@users.noreply.github.com>
2024-11-08 15:30:54 -0300

    implementação do Nav Bar

--- FrontEnd/imobiliaria/src/App.vue
+++ FrontEnd/imobiliaria/src/App.vue
@@ -1,12 +1,87 @@
+<script>
+import MenusNav from './components/MenusNav.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MenusNav
+  }
+}
+</script>
+
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
+    <nav class="NavBar">
+      <MenusNav />
+      <ul>
+        <li class="li__Home">
+          <RouterLink to="/" class="Anunce__Bnt">Anunciar Gratis</RouterLink>
+          <RouterLink to="/" class="Entry__Bnt">Entrar</RouterLink>
+        </li>
+      </ul>
     </nav>
   </header>
 
   <RouterView />
+
+  <section class="all">
+  </section>
 </template>
 
 <style scoped>
+a{
+  text-decoration: none;
+}
+.li__Home{
+  display: flex;
+  gap: 20px;
+}
+
+.Anunce__Bnt {
+  background-color: transparent;
+  border: none;
+  color: black;
+  padding: 1rem 0.5rem;
+  text-align: center;
+  text-decoration: none;
+  margin: 2px 1px;
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+}
+
+.Anunce__Bnt:hover {
+  background-color: #333;
+  color: #FFF;
+}
+
+.Entry__Bnt {
+  background-color: #3C3024;
+  border: none;
+  color: white; 
+  padding: 1rem 2rem;
+  text-align: center; 
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 2px 1px;
+  cursor: pointer;
+  border-radius: 4px;
+  transition: background-color 0.3s;
+}
+
+.Entry__Bnt:hover {
+  background-color: #605346;
+}
+
+.NavBar {
+  display: flex;
+  justify-content: space-around;
+}
+
+.all{
+  background-color: black;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ FrontEnd/imobiliaria/src/components/MenusNav.vue
@@ -0,0 +1,85 @@
+<template>
+    <header>
+        <nav>
+            <ul class="Nav__ul">
+                <RouterLink to="/">Home</RouterLink>
+                <li class="Nav__Li" @mouseover="showPopup" @mouseleave="hidePopup">Comprar</li>
+                <li class="Nav__Li" @mouseover="showPopup" @mouseleave="hidePopup">Alugar</li>
+                <li class="Nav__Li">Imovel Novo</li>
+                <li class="Nav__Li">Buscar Imobiliarias</li>
+            </ul>
+        </nav>
+    </header>
+    <div v-if="popupVisible" class="Popup">
+        <!-- Adicione os itens que você quiser aqui dentro -->
+        <p>Item 1</p>
+        <p>Item 2</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenusNav',
+    data() {
+        return {
+            popupVisible: false
+        };
+    },
+    methods: {
+        showPopup() {
+            this.popupVisible = true;
+        },
+        hidePopup() {
+            this.popupVisible = false;
+        }
+    }
+}
+</script>
+
+<style>
+
+.Nav__ul{
+  display: flex;
+  gap: 20px;
+}
+.Nav__Li {
+    color: #333;
+    display: inline-block;
+    padding-bottom: 2px;
+    position: relative;
+    gap: 20px;
+}
+
+.Nav__Li:hover{
+    cursor: pointer;
+}
+
+.Nav__Li:hover::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background-color: black;
+    cursor: pointer;
+}
+
+.Popup {
+    position: absolute;
+    top: 5%;
+    /* Posiciona abaixo do item de navegação */
+    left: 50%;
+    /* Centraliza horizontalmente */
+    transform: translate(-50%, +20px);
+    /* Ajusta a posição para centralizar */
+    width: 90%;
+    height: 200px;
+    background-color: white;
+    border: 1px solid black;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+    padding: 20px;
+    text-align: center;
+}
+</style>
