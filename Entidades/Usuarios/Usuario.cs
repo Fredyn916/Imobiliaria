5b18bc2578067d5a235b153af881ce24155ee8b0
Fred Marques <fredyn.curso@gmail.com>
2024-11-08 15:04:49 -0300

    Usuario

---
+++ Entidades/Usuarios/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Entidades.Usuarios;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public string Genero { get; set; }
+    public string Endereco { get; set; }
+    public string Identificacao { get; set; } // CPF, CNPJ, CNH, CTPS, RG, etc...
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Tipo { get; set; } // Administrador ou Usuário Convencional
+}
