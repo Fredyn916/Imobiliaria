318f2271c74af3eee02e10fbab0aab0cc2f093ae
guilhermesantos78 <163909949+guilhermesantos78@users.noreply.github.com>
2024-11-14 15:28:55 -0300

    as

--- FrontEnd/imobiliaria/src/views/VIewImovel.vue
+++ FrontEnd/imobiliaria/src/views/VIewImovel.vue
@@ -1,168 +1,172 @@
 <template>
-    <div class="container">
-      <h1>Filtros de imóveis</h1>
-      <div class="filter-container">
-        <label for="categoryFilter">Categoria:</label>
-        <select id="categoryFilter" v-model="selectedCategory" class="filter-select">
-          <option value="">Todas</option>
-          <option value="Estiloso">Estiloso</option>
-          <option value="Samsung">Samsung</option>
-        </select>
-      </div>
-  
-      <div class="filter-container">
-        <label for="searchBar">Pesquisar Imóvel:</label>
-        <input type="text" id="searchBar" v-model="searchQuery" @input="filterImoveis"
-          placeholder="Digite o nome do imóvel..." class="filter-input" />
-      </div>
-  
-      <ul class="product-list">
-        <li v-for="Imovel in filteredImoveis" :key="Imovel.id" class="product-item">
-          <strong>{{ Imovel.tipo }}</strong><br>
-          Preço: R$ {{ Imovel.preco }}<br>
-          Área: {{ Imovel.area }} m²<br>
-          CEP: {{ Imovel.cep }}<br>
-          Rua: {{ Imovel.rua }}<br>
-          Número: {{ Imovel.numero }}<br>
-          Bairro: {{ Imovel.bairro }}<br>
-          Cidade: {{ Imovel.cidade }}<br>
-          Unidade Federativa: {{ Imovel.unidadeFederativa }}<br>
-          Endereço: {{ Imovel.endereco }}<br>
-        </li>
-      </ul>
+  <div class="container">
+    <div class="filter-container">
+      <input type="text" id="searchBar" v-model="searchQuery" @input="filterImoveis"
+        placeholder="Digite o nome do imóvel..." class="filter-input" />
     </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'ViewImovel',
-    data() {
-      return {
-        imoveis: [],
-        searchQuery: "",
-        selectedCategory: "",
-        filteredImoveis: []
-      };
-    },
-    methods: {
-      async fetchImoveis() {
-        const apiUrl = "https://localhost:7082/Imovel/ListarImoveis";
-  
-        try {
-          const response = await fetch(apiUrl, {
-            method: "GET",
-            headers: {
-              Accept: "application/json",
-            },
-          });
-  
-          if (!response.ok) {
-            throw new Error('Erro ao buscar imóveis.');
-          }
-  
-          const data = await response.json();
-          this.imoveis = data;
-          this.filteredImoveis = data;
-        } catch (error) {
-          console.error('Erro ao carregar imóveis:', error);
-        }
-      },
-  
-      filterImoveis() {
-        this.filteredImoveis = this.imoveis.filter((imovel) => {
-          const matchesCategory = this.selectedCategory
-            ? imovel.tipo.toLowerCase().includes(this.selectedCategory.toLowerCase())
-            : true;
-          const matchesSearch = imovel.tipo.toLowerCase().includes(this.searchQuery.toLowerCase());
-  
-          return matchesCategory && matchesSearch;
+
+    <div class="filter-container">
+      <select id="categoryFilter" v-model="selectedCategory" class="filter-select">
+        <option value="">Todas</option>
+        <option value="Estiloso">Estiloso</option>
+        <option value="Fred">Fred</option>
+      </select>
+    </div>
+
+  </div>
+  <div class="Imovel__container">
+
+    <ul class="imovel__container__list">
+      <li v-for="Imovel in filteredImoveis" :key="Imovel.id" class="imovel__container__item">
+        <div class="Imovel__Container__internal__container">
+          <div class="Imovel__Container__box__Left"></div>
+          <div class="Imovel__Container__box__Right">
+            <strong> R$ {{ Imovel.preco }}</strong><br>
+            Rua: {{ Imovel.rua }}<br>
+            Endereço: {{ Imovel.endereco }}<br>
+            Número: {{ Imovel.numero }}<br>
+            Área: {{ Imovel.area }} m²<br>
+            CEP: {{ Imovel.cep }}<br>
+            Bairro: {{ Imovel.bairro }}<br>
+            Cidade: {{ Imovel.cidade }}<br>
+            Unidade Federativa: {{ Imovel.unidadeFederativa }}<br>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewImovel',
+  data() {
+    return {
+      imoveis: [],
+      searchQuery: "",
+      selectedCategory: "",
+      filteredImoveis: []
+    };
+  },
+  methods: {
+    async fetchImoveis() {
+      const apiUrl = "https://localhost:7082/Imovel/ListarImoveis";
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
         });
+
+        if (!response.ok) {
+          throw new Error('Erro ao buscar imóveis.');
+        }
+
+        const data = await response.json();
+        this.imoveis = data;
+        this.filteredImoveis = data;
+      } catch (error) {
+        console.error('Erro ao carregar imóveis:', error);
       }
     },
-    watch: {
-      selectedCategory() {
-        this.filterImoveis();
-      },
-      searchQuery() {
-        this.filterImoveis();
-      }
+
+    filterImoveis() {
+      this.filteredImoveis = this.imoveis.filter((imovel) => {
+        const matchesCategory = this.selectedCategory
+          ? imovel.tipo.toLowerCase().includes(this.selectedCategory.toLowerCase())
+          : true;
+        const matchesSearch = imovel.tipo.toLowerCase().includes(this.searchQuery.toLowerCase());
+
+        return matchesCategory && matchesSearch;
+      });
+    }
+  },
+  watch: {
+    selectedCategory() {
+      this.filterImoveis();
     },
-    mounted() {
-      this.fetchImoveis();
+    searchQuery() {
+      this.filterImoveis();
     }
-  };
-  </script>
-  
-  <style scoped>
-  .container {
-    font-family: 'Arial', sans-serif;
-    padding: 20px;
-    background-color: #f9f9f9;
-    max-width: 800px;
-    margin: 0 auto;
+  },
+  mounted() {
+    this.fetchImoveis();
   }
-  
-  h1 {
-    text-align: center;
-    color: #333;
-    margin-bottom: 20px;
-  }
-  
-  .filter-container {
-    margin-bottom: 20px;
-    display: flex;
-    flex-direction: column;
-  }
-  
-  label {
-    font-weight: bold;
-    color: #333;
-    margin-bottom: 5px;
-  }
-  
-  .filter-select,
-  .filter-input {
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    font-size: 16px;
-  }
-  
-  .filter-select {
-    cursor: pointer;
-  }
-  
-  .product-list {
-    list-style-type: none;
-    padding: 0;
-  }
-  
-  .product-item {
-    background-color: #fff;
-    padding: 15px;
-    margin-bottom: 10px;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .product-item:hover {
-    cursor: pointer;
-  }
-  
-  .product-item strong {
-    color: #333;
-  }
-  
-  @media (max-width: 600px) {
-    .container {
-      padding: 10px;
-    }
-  
-    .filter-select,
-    .filter-input {
-      width: 100%;
-      box-sizing: border-box;
-    }
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding-left: 5rem;
+  height: 100px;
+  gap: 10px;
+}
+
+.filter-input {
+  padding: 1rem 2rem;
+  border-radius: 8px;
+}
+
+.filter-select {
+  padding: 15px 34px 14px 14px;
+  border: 1px solid #7c98a7;
+  border-radius: 8px;
+  background: #fff;
+  font-family: Hind, sans-serif;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 17px;
+  color: #000;
+  user-select: none;
+  cursor: pointer;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.Imovel__container {
+  height: auto;
+  width: 100%;
+}
+
+.Imovel__Container__internal__container {
+  display: flex;
+}
+
+.Imovel__Container__box__Left {
+  flex: 1;
+  border: 1px solid black;
+  max-width: 30%;
+  max-height: 100%;
+}
+
+.Imovel__Container__box__Right {
+  flex: 1;
+  border: 1px solid black;
+  max-width: 70%;
+  padding: 2rem;
+}
+
+.imovel__container__list {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+}
+
+.imovel__container__item {
+  width: 90%;
+  max-height: 400px;
+  height: auto;
+  border: 5px solid #ededed;
+  color: #000;
+  border-radius: 10px;
+}
+</style>
