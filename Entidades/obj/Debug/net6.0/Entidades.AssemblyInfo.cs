4c3c784d2a98494a743c8a110fd8961b03c439a9
Guilherme santos <163909949+guilhermesantos78@users.noreply.github.com>
2024-11-14 21:40:17 -0300

    s

--- Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e313c18b061f1a244596fec640b96630c1abe30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a811bd904d32de6bbcfc4857bcf67751862455d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
