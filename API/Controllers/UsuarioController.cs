d9347f788b3b44ecba7e474104dd5b12db60f8a1
Fred Marques <fredyn.curso@gmail.com>
2024-12-09 13:30:37 -0300

    Revert "Merge branch 'Fred' of https://github.com/Fredyn916/Imobiliaria into Fred"
    This reverts commit 0e2cae9356fdea587db42741084feb9dd127a29c, reversing
    changes made to b86a38423222c28ee94206bc29699a8b4ed43e35.

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using CloudinaryDotNet.Actions;
+using CloudinaryDotNet;
 using Entidades.DTOs.Usuarios;
 using Entidades.Interfaces.Usuarios;
 using Entidades.Usuarios;
@@ -18,15 +20,21 @@ public class UsuarioController : ControllerBase
         _Service = usuarioService;
         _Mapper = mapper;
     }
-    
+
     [HttpPost("AdicionarUsuario")]
-    public async Task Adicionar([FromForm] CreateUsuarioDTO usuarioDTO, byte[] imageData, string fileName)
+    public ReturnUsuarioIdDTO Adicionar(CreateUsuarioDTO usuarioDTO)
     {
         Usuario usuario = _Mapper.Map<Usuario>(usuarioDTO);
         usuario.Endereco = $"{usuarioDTO.Rua}, {usuarioDTO.Numero}, {usuarioDTO.Bairro}, {usuarioDTO.Cidade}, {usuarioDTO.UnidadeFederativa} - {usuarioDTO.CEP}";
         usuario.Tipo = "Usuario";
 
-        await _Service.Adicionar(usuario, imageData, fileName);
+        return _Service.Adicionar(usuario);
+    }
+
+    [HttpPut("UploadImage")]
+    public async Task<string> UploadImage(IFormFile imagem, int usuarioId)
+    {
+        return _Service.UploadImage(imagem, usuarioId).ToString();
     }
 
     [HttpPost("LogarUsuario")]
